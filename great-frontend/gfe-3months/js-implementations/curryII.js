1a11d3e757026e329b42208edd9505d02b4be3cd
Mohak-Trivedi <trivedimohak@gmail.com>
2025-10-14 23:25:38 +0530

    curry 2

--- great-frontend/gfe-3months/js-implementations/curryII.js
+++ great-frontend/gfe-3months/js-implementations/curryII.js
@@ -4,22 +4,37 @@
  * @param {Function} func
  * @return {Function}
  */
+// export default function curry(func) {
+//   return function curriedFunc(...args) {
+//     // Filter out any empty calls (no arguments provided)
+//     const filteredArgs = args.filter((arg) => arg !== undefined);
+
+//     if (filteredArgs.length >= func.length) {
+//       // Apply the original function with the correct 'this' context
+//       return func.apply(this, filteredArgs);
+//     } else {
+//       function intermediaryFunc(...nextArgs) {
+//         // Preserve 'this' context using .bind(this)
+//         return curriedFunc.apply(this, [...filteredArgs, ...nextArgs]);
+//       }
+
+//       // to preserve the context of this ACROSS MULTIPLE calls of intermediary function
+//       return intermediaryFunc.bind(this);
+//     }
+//   };
+// }
+
+
+/**
+ * @param {Function} func
+ * @return {Function}
+ */
 export default function curry(func) {
-  return function curriedFunc(...args) {
-    // Filter out any empty calls (no arguments provided)
-    const filteredArgs = args.filter((arg) => arg !== undefined);
-
-    if (filteredArgs.length >= func.length) {
-      // Apply the original function with the correct 'this' context
-      return func.apply(this, filteredArgs);
-    } else {
-      function intermediaryFunc(...nextArgs) {
-        // Preserve 'this' context using .bind(this)
-        return curriedFunc.apply(this, [...filteredArgs, ...nextArgs]);
-      }
-
-      // to preserve the context of this ACROSS MULTIPLE calls of intermediary function
-      return intermediaryFunc.bind(this);
+  return function curried(...args) {
+    if(args.length >= func.length) {
+      return func.apply(this, args);
     }
-  };
+
+    return curried.bind(this, ...args);
+  }
 }
