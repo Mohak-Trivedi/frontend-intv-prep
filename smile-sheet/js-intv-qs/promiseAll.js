237caa2793ebf6abdeccf32bf4acdb64d99651c3
Mohak-Trivedi <trivedimohak@gmail.com>
2025-11-18 11:01:11 +0530

    promiseAll Promise-then implementation

--- smile-sheet/js-intv-qs/promiseAll.js
+++ smile-sheet/js-intv-qs/promiseAll.js
@@ -40,3 +40,46 @@ export default function promiseAll(iterable) {
     });
   });
 }
+
+// Approach 2: Using Promise.then (in case interviewer does not allow async/await)
+/**
+ * @param {Array} iterable
+ * @return {Promise<Array>}
+ */
+export default function promiseAll(iterable) {
+  return new Promise((resolve, reject) => {
+    const results = new Array(iterable.length);
+
+    // How will we come to know that all the values of iterable have been resolved?
+    // We can track them by initializing a counter of unresolved values and decrement it
+    // whenever a value is resolved.
+    let unresolved = iterable.length;
+
+    // Resolve with empty array if the iterable is empty.
+    if (unresolved === 0) {
+      resolve(results);
+      return;
+    }
+
+    // Iterate with forEach by mentioning value as well as index because we don't just need
+    // to return the results array but it must be in the same order in which the corresponding
+    // inputs are present in the iterable.
+    iterable.forEach((val, idx) => {
+      Promise.resolve(val).then(
+        (resolvedVal) => {
+          results[idx] = resolvedVal;
+          unresolved -= 1;
+
+          if(unresolved === 0) {
+            resolve(results);
+          }
+        },
+        (reason) => {
+          // If any value is rejected, we must reject the top-level promise immediately without
+          // waiting for any other pending promises.
+          reject(reason);
+        }
+      );
+    });
+  });
+}
