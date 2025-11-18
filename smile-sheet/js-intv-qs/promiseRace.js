383f6d2a0071ff88e79e34f296e43d8b11a49352
Mohak-Trivedi <trivedimohak@gmail.com>
2025-11-18 12:18:05 +0530

    promiseRace using Promise-then

--- smile-sheet/js-intv-qs/promiseRace.js
+++ smile-sheet/js-intv-qs/promiseRace.js
@@ -23,3 +23,29 @@ export default function promiseRace(iterable) {
     });
   });
 }
+
+// Approach 2: Using Promise.then (in case async-await is not allowed)
+/**
+ * @param {Array} iterable
+ * @return {Promise}
+ */
+export default function promiseRace(iterable) {
+  return new Promise((resolve, reject)=> {
+    // if the input array is empty, return a forever-pending promise.
+    // i.e. return without calling resolve() or reject()
+    if(iterable.length === 0) {
+      return;
+    }
+
+    iterable.forEach(async (val) => {
+      Promise.resolve(val).then(
+        (resolvedVal) => {
+          resolve(resolvedVal);
+        },
+        (reason) => {
+          reject(reason);
+        }
+      );
+    });
+  });
+}
