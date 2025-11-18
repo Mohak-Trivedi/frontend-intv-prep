7afd6785c87c728abe4677aa49c8ac94390930b4
Mohak-Trivedi <trivedimohak@gmail.com>
2025-11-18 14:01:45 +0530

    promiseAny using Promise-then

--- smile-sheet/js-intv-qs/promiseAny.js
+++ smile-sheet/js-intv-qs/promiseAny.js
@@ -34,3 +34,40 @@ export default function promiseAny(iterable) {
     });
   });
 }
+
+// Approach 2: Using Promise.then (if async-await is not allowed):
+/**
+ * @param {Array} iterable
+ * @return {Promise}
+ */
+export default function promiseAny(iterable) {
+  return new Promise((resolve, reject) => {
+    // We need to reject with new AggregateError(errors) where 'errors' must be an array
+    // of errors in the same order as in iterable, if all the promises are rejected.
+    // So, to store the errors in the same order, use an array
+    const errors = new Array(iterable.length);
+    // And to track if all the promises are rejected, maintain a rejection counter.
+    let rejectCount = 0;
+
+    // If an empty iterable is passed, then the promise returned by this method is rejected synchronously.
+    // The rejected reason is an AggregateError object whose errors property is an empty array.
+    if(iterable.length === 0) {
+      reject(new AggregateError([]));
+    }
+
+    iterable.forEach(async (val, idx) => {
+      Promise.resolve(val).then(
+        (resolvedVal) => {
+          resolve(val);
+        },
+        (reason) => {
+          errors[idx] = reason;
+          rejectCount++;
+          if(rejectCount === iterable.length) {
+            reject(new AggregateError(errors));
+          }
+        }
+      );
+    });
+  });
+}
