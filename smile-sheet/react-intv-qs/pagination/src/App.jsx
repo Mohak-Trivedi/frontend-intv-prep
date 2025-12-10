659020ece59479be6a9366a28e9c12886d93a45f
Mohak-Trivedi <trivedimohak@gmail.com>
2025-12-10 12:43:53 +0530

    pagination

--- smile-sheet/react-intv-qs/pagination/src/App.jsx
+++ smile-sheet/react-intv-qs/pagination/src/App.jsx
@@ -1,35 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const PAGE_SIZE = 10;
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+function App() {
+  const [products, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const totalProducts = products.length;
+  const noOfPages = Math.ceil(totalProducts / PAGE_SIZE);
+  const start = currentPage * PAGE_SIZE;
+  const end = start + PAGE_SIZE;
+
+  async function fetchData() {
+    try {
+      const data = await fetch("https://dummyjson.com/products?limit=500");
+      const json = await data.json();
+      setProducts(json.products);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  function handlePageChange(pageNumber) {
+    setCurrentPage(pageNumber);
+  }
+
+  function goToPrevPage() {
+    setCurrentPage((prev) => prev - 1);
+  }
+
+  function goToNextPage() {
+    setCurrentPage((prev) => prev + 1);
+  }
+
+  return !products.length ? (<h1>No Products Found</h1>) : (
+    <div className='app'>
+      <h1>Pagination</h1>
+
+      <div className='products-container'>
+        {products.slice(start, end).map((product) => {
+          return (
+            <ProductCard key={product.id} image={product.thumbnail} title={product.title} />
+          );
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className='pagination-container'>
+        <button className='page-number' disabled={currentPage===0} onClick={goToPrevPage}>⬅️</button>
+        {[...Array(noOfPages).keys()].map((pageNumber) => (
+          <button key={pageNumber} className={'page-number' + ((pageNumber===currentPage) ? ' active' : '')} onClick={() => handlePageChange(pageNumber)}>
+            {pageNumber}
+          </button>
+        ))}
+        <button className='page-number' disabled={currentPage===noOfPages-1} onClick={goToNextPage}>➡️</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
+}
+
+function ProductCard({ image, title }) {
+  return (
+    <div className='product-card'>
+      <img src={image} alt={title} className='product-img' />
+      <span>{title}</span>
+    </div>
+  );
 }
 
 export default App
