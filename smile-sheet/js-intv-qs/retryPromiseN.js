3b6af60bc22ce90987a055900ab78aea40633b03
Mohak-Trivedi <trivedimohak@gmail.com>
2026-01-15 18:48:05 +0530

    retry promises n times - async await approach

--- smile-sheet/js-intv-qs/retryPromiseN.js
+++ smile-sheet/js-intv-qs/retryPromiseN.js
@@ -1,18 +1,31 @@
 // Problem: https://learnersbucket.com/examples/interview/retry-promises-n-number-of-times-in-javascript/
 
-// .then() based:
 function wait(delay) {
     return new Promise((resolve, reject) => {
         setTimeout(() => resolve(), delay);
     });
 }
 
-function retryWithDelay(operation, retries = 3, delay = 50, finalError = 'Retry Failed') {
-    return new Promise((resolve, reject) => {
-        operation().then(resolve).catch((reason) => {
-            if(retries <= 0) reject(finalError);
+// .then() based:
+// function retryWithDelay(operation, retries = 3, delay = 50, finalError = 'Retry Failed') {
+//     return new Promise((resolve, reject) => {
+//         operation().then(resolve).catch((reason) => {
+//             if(retries <= 0) reject(finalError);
 
-            wait(delay).then(() => retryWithDelay(operation, retries - 1, delay, finalError)).then(resolve).catch(reject);
-        });
-    });
+//             wait(delay).then(() => retryWithDelay(operation, retries - 1, delay, finalError)).then(resolve).catch(reject);
+//         });
+//     });
+// }
+
+// async await based:
+async function retryWithDelay(operation, retries = 3, delay = 50, finalError = 'Retry Failed') {
+    try {
+        await operation();
+    } catch(err) {
+        if(retries <= 0) return Promise.reject(finalError);
+
+        await wait(delay);
+
+        return retryWithDelay(operation, retries - 1, delay, finalError);
+    }
 }
