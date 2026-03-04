38e95ec0fca61ba9ab7c0d5b5d25d1eb696e886c
Mohak-Trivedi <trivedimohak@gmail.com>
2026-03-04 20:50:33 +0530

    search - basic version

--- smile-sheet/react-intv-qs/search-bar/src/App.jsx
+++ smile-sheet/react-intv-qs/search-bar/src/App.jsx
@@ -1,20 +1,25 @@
+import "./App.css";
+import { Users } from "./users";
 import { useState } from "react";
 
-import "./App.css";
-import { SearchBar } from "./components/SearchBar";
-import { SearchResultsList } from "./components/SearchResultsList";
-
-function App() {
-  const [results, setResults] = useState([]);
+export default function App() {
+  const [query, setQuery] = useState("");
 
   return (
-    <div className="App">
-      <div className="search-bar-container">
-        <SearchBar setResults={setResults} />
-        {results && results.length > 0 && <SearchResultsList results={results} />}
-      </div>
+    <div className="app">
+      <input
+        type="text"
+        placeholder="Search..."
+        className="search"
+        onChange={e => setQuery(e.target.value)}
+      />
+      <ul className="list">
+        {Users.filter(user => user.first_name.toLowerCase().includes(query)).map((user) => {
+          return (
+            <li key={user.id} className="listItem">{user.first_name}</li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
-
-export default App;
